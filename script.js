4b29517d9b8814cdd6a79a32d22ebd96e0ac55b7
clientesweb <alexisifran32@gmail.com>
2024-09-27 14:08:40 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -289,7 +289,7 @@ sendMessageBtn.addEventListener('click', function() {
     const message = whatsappMessage.value.trim();
     if (message) {
         // Formatear el mensaje para enviar a WhatsApp
-        const phoneNumber = '+593978606269'; // Cambia este número por el de CaféClubTV
+        const phoneNumber = '+593999472777'; // Cambia este número por el de CaféClubTV
         const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
         
         // Abrir el enlace en una nueva ventana/pestaña
