12599abde293a7482d62584c4670344a5727b25b
clientesweb <alexisifran32@gmail.com>
2024-09-29 22:12:33 -0300

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,14 +6,15 @@ const RESOURCES_TO_CACHE = [
   '/script.js',
   '/images/android-chrome-192x192.png',
   '/images/android-chrome-512x512.png',
+  '/images/apple-touch-icon.png',
   '/images/logo.png',
   '/images/banner.jpg',
   '/images/banner1.jpg',
   '/images/banner2.jpg',
-  '/images/banner3.jpg'
+  '/images/banner3.jpg',
+  '/offline.html'
 ];
 
-// Instalación del Service Worker y almacenamiento en caché estático
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -23,7 +24,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Activación del Service Worker y limpieza de cachés antiguas
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -39,25 +39,20 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Respuesta a las solicitudes
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) {
-        // Si hay una respuesta en caché, devolverla
         return response;
       }
-      // Si no está en caché, buscar en la red
       return fetch(event.request).then((fetchResponse) => {
         return caches.open(CACHE_NAME).then((cache) => {
-          // Almacenar el recurso recién obtenido en caché
           cache.put(event.request, fetchResponse.clone());
           return fetchResponse;
         });
       }).catch(() => {
-        // Si falla tanto la red como la caché, puedes devolver una respuesta de fallback
-        if (event.request.destination === 'document') {
-          return caches.match('/offline.html'); // Puedes crear una página 'offline.html' para mostrar cuando no haya conexión
+        if (event.request.mode === 'navigate') {
+          return caches.match('/offline.html');
         }
       });
     })
